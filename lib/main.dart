227f1d2b46982f4dd890bb8a997806757bfb0150
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2025-05-06 15:21:10 +0530

    changing architecture of app and enhancing app ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:gemini_app/screens/auth/login.dart';
-import 'package:gemini_app/services/firebaseService.dart';
-import 'package:gemini_app/services/services.dart';
+import 'package:gemini_app/presentation/pages/homePage.dart';
 
-import 'firebase_options.dart';
-
-var service = Services();
-var firebase = FirebaseService();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  service.initTTS();
+  // await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -25,13 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gem AI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:LoginPage(),
+      home:HomePage(),
     );
   }
 }
