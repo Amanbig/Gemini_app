91336275787ff1a556030eb1e65231c2fac33ca9
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2025-05-07 15:06:25 +0530

    added gpt_markdown

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:gemini_app/presentation/pages/authPage.dart';
 import 'package:gemini_app/presentation/pages/chatPage.dart';
 import 'package:gemini_app/presentation/pages/historyPage.dart';
 import 'package:gemini_app/presentation/pages/homePage.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await dotenv.load();
+  Gemini.init(apiKey: 'YOUR_API_KEY');
   runApp(const MyApp());
 }
 
